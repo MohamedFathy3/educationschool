840015eac653259cbc8c9d8c6af6b76765e31b9a
ahmed abdullah <abdallah@pyramidsfreight.com>
2025-10-02 16:23:59 +0300

    d

--- routes/api.php
+++ routes/api.php
@@ -205,7 +205,7 @@ Route::middleware('auth:sanctum')->group(function () {
 //////////////////////////////////////////////////////////admin Message//////////////////////////////////////
 Route::post('admin/messages/send', [AdminMessageController::class, 'sendMessage']);
 Route::get('admin/messages', [AdminMessageController::class, 'getMessages']);
-Route::put('admin/messages/{id}/read', [AdminMessageController::class, 'markAsRead']);
+Route::post('admin/messages/{id}/read', [AdminMessageController::class, 'markAsRead']);
 //////////////////////////////////////////////////////////admin Message//////////////////////////////////////
 
 
